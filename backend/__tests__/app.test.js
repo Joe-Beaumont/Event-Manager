fed9e3b49006affd3128d6da8b0bff24b99c3199
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-09-29 18:45:15 +0100

    First two event endpoints working

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,13 +1,47 @@
+const request = require("supertest");
 const db = require("../db/connection.js");
 const app = require("../server.js");
-const data = require("../db/data/test_data")
-const seed = require("../db/seeds/seed.js")
+const data = require("../db/data/test_data");
+const seed = require("../db/seeds/seed.js");
+require("jest-sorted");
 
-beforeAll(() => seed(data));
+beforeEach(() => seed(data));
 afterAll(() => db.end());
 
-describe("", () => {
-    test("", () => {
+describe("GET /api/events", () => {
+    test("200: responds with an array of events", () => {
+        return request(app)
+        .get("/api/events")
+        .expect(200)
+        .then(({ body }) => {
+            const { events } = body
+            expect(Array.isArray(events)).toBe(true);
+            expect(events.length).toBe(10);
+            events.forEach((event) => {
+                expect(event).toHaveProperty('event_id');
+                expect(event).toHaveProperty('name');
+                expect(event).toHaveProperty('description');
+                expect(event).toHaveProperty('date');
+                expect(event).toHaveProperty('time');
+                expect(event).toHaveProperty('created_by');
+            });
+        });
+    });
+});
 
-    })
+describe("GET /api/events/:event_id", () => {
+    test("200: responds with an object for a specific event", () => {
+        return request(app)
+        .get("/api/events/3")
+        .expect(200)
+        .then(({ body }) => {
+            const { event } = body
+            expect(event.event_id).toBe(3);
+            expect(event.name).toBe('Cloud Computing Bootcamp');
+            expect(event.description).toBe('Hands-on AWS, Azure, GCP labs');
+            expect(event.date).toBe('2025-06-07');
+            expect(event.time).toBe('11:00:00');
+            expect(event.created_by).toBe(1);
+        });
+    });
 });
--- backend/db/seeds/seed.js
+++ backend/db/seeds/seed.js
@@ -1,9 +1,6 @@
 const db = require("../connection");
 const format = require("pg-format");
 
-
-//Need to fill in table names and data variable params
-// Split create and populate queries into functions
 const seed = ({ userData, eventData, attendingData }) => {
     return db.query("DROP TABLE IF EXISTS attending;")
     .then(() => {
