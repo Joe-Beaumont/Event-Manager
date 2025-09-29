facb7b8f35e048f7c94035ecf9eef469c2c333dc
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-09-29 16:15:53 +0100

    Created tables for users, events and attending using TDD and populated them with generated test-data

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,6 +1,7 @@
 const db = require("../db/connection.js");
 const app = require("../server.js");
 const data = require("../db/data/test_data")
+const seed = require("../db/seeds/seed.js")
 
 beforeAll(() => seed(data));
 afterAll(() => db.end());
