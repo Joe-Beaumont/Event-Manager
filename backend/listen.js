782f1f8abdb89e19639d43f41256961666559187
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-10-05 17:29:53 +0100

    Created listen.js and made appropriate changes to json etc

--- backend/listen.js
+++ backend/listen.js
@@ -1,11 +1,4 @@
-const app = require("./server");
-
+const app = require('./server');
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-    if(err){
-        console.log(err)
-    } else {
-        console.log(`listening on ${PORT}`)
-    }
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
