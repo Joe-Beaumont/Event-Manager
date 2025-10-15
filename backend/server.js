6cefd0c6854bf2b793387cc4366c2af3cfbaea3b
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-10-15 20:52:40 +0100

    added redirection path on listen.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -12,6 +13,14 @@ const apiRouter = require("./routers/api.router")
 app.use("/api", apiRouter)
 const googleAuthRoutes = require("./routers/googleAuth.router");
 app.use("/auth", googleAuthRoutes);
+
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
 
 // Error Handling middleware
 
